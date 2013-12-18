18681c8ce86a0455305e70b654205c86d86bfd82
Denis Rangel <denis.rangel@gmail.com>
2013-12-18 11:59:38 -0300

    user ok

--- frontserver/app/models/user.rb
+++ frontserver/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_and_belongs_to_many :hospitals
+
+  accepts_nested_attributes_for :hospitals
 end
--- frontserver/config/routes.rb
+++ frontserver/config/routes.rb
@@ -1,5 +1,7 @@
 Frontserver::Application.routes.draw do
   devise_for :users
+  resources :users
+
   resources :cids
 
   resources :causes
@@ -14,7 +16,7 @@ Frontserver::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'hospitals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
